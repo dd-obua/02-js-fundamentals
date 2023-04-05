5752673c3737fd46fec950ff709484f05ef4a50f
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-05 18:22:45 +0300

    Include code for function expression

--- js/fnDeclarationFnExpression.js
+++ js/fnDeclarationFnExpression.js
@@ -6,4 +6,11 @@ function calculateAge1(birthYear) {
 }
 
 const age1 = calculateAge1(1993);
-console.log(age1);
+console.log("Age:", age1);
+
+// Function expression
+const calculateAge2 = function (birthYear) {
+  return currentYear - birthYear;
+};
+const age2 = calculateAge2(1993);
+console.log("Age:", age2);
