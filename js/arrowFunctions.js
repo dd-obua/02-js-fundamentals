c3bbf8467e50ba1c9ccd0d06cee69f8b15f084d6
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-05 18:47:16 +0300

    Add another arrow function. A code block is required in this case (since it is not a one liner)

--- js/arrowFunctions.js
+++ js/arrowFunctions.js
@@ -1,8 +1,21 @@
 // Arrow function, a special form of function expression
 
 const currentYear = 2023;
-
+// A simple arrow function
 const calculateAge3 = (birthYear) => currentYear - birthYear;
 
 const age3 = calculateAge3(1993);
 console.log("Age:", age3);
+
+//
+const retirementAge = 60;
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = currentYear - birthYear;
+  const yearsRemaining = retirementAge - age;
+  return `${firstName} retires in ${yearsRemaining} years.`;
+};
+
+console.log(yearsUntilRetirement(1993, "Denis"));
+console.log(yearsUntilRetirement(1992, "Stella"));
+console.log(yearsUntilRetirement(1990, "Joe"));
