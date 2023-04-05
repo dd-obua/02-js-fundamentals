4860dac605be56408263eb411cc1742d24f406fc
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-05 15:04:51 +0300

    Add more function calls

--- js/functions.js
+++ js/functions.js
@@ -5,10 +5,15 @@ function logger() {
 logger();
 
 function fruitProcessor(apples, oranges) {
-  console.log(apples, oranges);
   const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
   return juice;
 }
 
 const appleJuice = fruitProcessor(5, 0);
 console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const orangeJuice = fruitProcessor(0, 4);
+console.log(orangeJuice);
