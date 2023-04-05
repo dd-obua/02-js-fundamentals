275a07acc73365f1fbe0eeb00bae0777af5a5c75
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-05 18:17:35 +0300

    Include code for function declaration

---
+++ js/fnDeclarationFnExpression.js
@@ -0,0 +1,9 @@
+const currentYear = 2023;
+
+// Function declaration
+function calculateAge1(birthYear) {
+  return currentYear - birthYear;
+}
+
+const age1 = calculateAge1(1993);
+console.log(age1);
