f80c8e4bd6ddd021779aa9a106dd57a508dfe065
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-05 14:55:07 +0300

    Add a function that takes paramenters and return values

--- js/functions.js
+++ js/functions.js
@@ -3,3 +3,12 @@ function logger() {
 }
 
 logger();
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
