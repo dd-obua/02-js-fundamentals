013a404d454c18f42a17a6d995b6c0d1481e9d4a
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-05 18:33:06 +0300

    Include code showing an example of an arrow function

---
+++ js/arrowFunctions.js
@@ -0,0 +1,8 @@
+// Arrow function, a special form of function expression
+
+const currentYear = 2023;
+
+const calculateAge3 = (birthYear) => currentYear - birthYear;
+
+const age3 = calculateAge3(1993);
+console.log("Age:", age3);
