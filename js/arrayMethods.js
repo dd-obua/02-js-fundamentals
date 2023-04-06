665194b3dc5b9182b9285af6ab8bdcf783a12b23
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-06 12:34:14 +0300

    Include code for removing the last array element

--- js/arrayMethods.js
+++ js/arrayMethods.js
@@ -11,3 +11,9 @@ console.log(`${numberOfFriends} friends:`, friends);
 // Adding element(s) to the beginning of an array
 numberOfFriends = friends.unshift("Morish");
 console.log(`${numberOfFriends} friends:`, friends);
+
+// Removing the last element of an array
+let lostFriend = friends.pop();
+numberOfFriends = friends.length;
+console.log(`I just lost ${lostFriend}`);
+console.log(`${numberOfFriends} friends:`, friends);
