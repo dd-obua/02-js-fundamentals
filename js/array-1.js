62a423d65b8a5190d0fcead5a0bf7a2af9a9e066
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-06 12:11:33 +0300

    Include code for manipulating functions and arrays

--- js/array-1.js
+++ js/array-1.js
@@ -16,3 +16,29 @@ friends[2] = "Roy";
 const firstName = "Denis";
 const denis = [firstName, "Obua", 1993, "Software engineer (Web developer)"];
 console.log(denis);
+
+// Exercise
+const currentYear = new Date().getFullYear();
+
+const calcAge = function (birthYear) {
+  return currentYear - birthYear;
+};
+
+const bthYrs = [1990, 1992, 1993, 1996, 1998, 2000];
+const age1 = calcAge(bthYrs[0]);
+const age2 = calcAge(bthYrs[1]);
+const age3 = calcAge(bthYrs[2]);
+const age4 = calcAge(bthYrs[3]);
+const age5 = calcAge(bthYrs[4]);
+
+const ages = [
+  calcAge(bthYrs[0]),
+  calcAge(bthYrs[1]),
+  calcAge(bthYrs[2]),
+  calcAge(bthYrs[3]),
+  calcAge(bthYrs[4]),
+];
+
+for (let i = 0; i < ages.length; i++) {
+  console.log(`${bthYrs[i]}: ${ages[i]}`);
+}
