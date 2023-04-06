58bdb0e41cdcc68e9634615152fc49c01c10ab6d
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-06 12:54:23 +0300

    Add explanatory comments

--- js/arrayMethods.js
+++ js/arrayMethods.js
@@ -5,21 +5,21 @@ let numberOfFriends = friends.length;
 console.log(`I have ${numberOfFriends} friends:`, friends);
 
 // Adding element(s) to the end of an array
-numberOfFriends = friends.push("Roy"); // push method returns the array length
+numberOfFriends = friends.push("Roy"); // push method returns the current array length
 console.log(`I have ${numberOfFriends} friends:`, friends);
 
 // Adding element(s) to the beginning of an array
-numberOfFriends = friends.unshift("Morish");
+numberOfFriends = friends.unshift("Morish"); // unshift method returns the current array length
 console.log(`I have ${numberOfFriends} friends:`, friends);
 
 // Removing the first array element
-lostFriend = friends.shift();
+lostFriend = friends.shift(); // returns the element that was removed
 console.log(`I just lost ${lostFriend}.`);
 numberOfFriends = friends.length;
 console.log(`I have ${numberOfFriends} friends:`, friends);
 
 // Removing the last element of an array
-let lostFriend = friends.pop();
+let lostFriend = friends.pop(); // returns the element that was removed
 numberOfFriends = friends.length;
 console.log(`I just lost ${lostFriend}.`);
 console.log(`I have ${numberOfFriends} friends:`, friends);
