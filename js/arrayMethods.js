348f32b8e743080648697d914d34c0bdb92f6ff1
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-06 12:47:28 +0300

    Include code for the first element of an array

--- js/arrayMethods.js
+++ js/arrayMethods.js
@@ -2,18 +2,24 @@
 
 const friends = ["Geoffrey", "Peter", "Richard"];
 let numberOfFriends = friends.length;
-console.log(`${numberOfFriends} friends:`, friends);
+console.log(`I have ${numberOfFriends} friends:`, friends);
 
 // Adding element(s) to the end of an array
 numberOfFriends = friends.push("Roy"); // push method returns the array length
-console.log(`${numberOfFriends} friends:`, friends);
+console.log(`I have ${numberOfFriends} friends:`, friends);
 
 // Adding element(s) to the beginning of an array
 numberOfFriends = friends.unshift("Morish");
-console.log(`${numberOfFriends} friends:`, friends);
+console.log(`I have ${numberOfFriends} friends:`, friends);
 
 // Removing the last element of an array
 let lostFriend = friends.pop();
 numberOfFriends = friends.length;
-console.log(`I just lost ${lostFriend}`);
-console.log(`${numberOfFriends} friends:`, friends);
+console.log(`I just lost ${lostFriend}.`);
+console.log(`I have ${numberOfFriends} friends:`, friends);
+
+// Removing the first array element
+lostFriend = friends.shift();
+console.log(`I just lost ${lostFriend}.`);
+numberOfFriends = friends.length;
+console.log(`I have ${numberOfFriends} friends:`, friends);
