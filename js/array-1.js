6deb7542715a986b595c499aa4ef46e13f216f5f
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-06 11:31:40 +0300

    Add code for implementation of arrays

---
+++ js/array-1.js
@@ -0,0 +1,14 @@
+const friends = ["Peter", "Geoffrey", "Richard"];
+console.log(friends);
+console.log(friends[1]);
+
+const birthYears = new Array(1990, 1992, 1993);
+console.log(birthYears);
+console.log("Element at index 0:", birthYears[0]);
+console.log("Element at index 2:", birthYears[2]);
+
+console.log("Array length:", friends.length);
+console.log("Last array element:", friends[friends.length - 1]); // returns the last element of the array
+
+friends[2] = "Roy";
+  
