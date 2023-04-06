c57872af066986bc587f3cbe94621d6eefe210a7
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-06 12:24:33 +0300

    Include code for adding elements to the end of an array

---
+++ js/arrayMethods.js
@@ -0,0 +1,8 @@
+// Array methods
+
+const friends = ["Geoffrey", "Peter", "Richard"];
+let numberOfFriends = friends.length;
+console.log(`${numberOfFriends} friends:`, friends);
+
+numberOfFriends = friends.push("Roy"); // push method returns the array length
+console.log(`${numberOfFriends} friends:`, friends);
