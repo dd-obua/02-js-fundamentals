17ec9cdba46693977a3f75bb6371473b3770b7e7
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-06 13:28:46 +0300

    Add code for calculating tip value using a function

---
+++ challenges/challenge2-tipCalculator.js
@@ -0,0 +1,9 @@
+// Tip calculator challenge
+
+const bill = 100;
+
+const calcTip = (bill) =>
+  bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+const tip = calcTip(bill);
+console.log(`Bill: $100, Tip: $${tip}`);
