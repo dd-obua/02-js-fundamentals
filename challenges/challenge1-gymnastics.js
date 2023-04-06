340cf916792a809bb503384816f7fb5f03a6415c
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-06 11:07:57 +0300

    Include the second test data

--- challenges/challenge1-gymnastics.js
+++ challenges/challenge1-gymnastics.js
@@ -5,16 +5,18 @@ const calcAverage = (x, y, z) => (x + y + z) / 3;
 let averageDolphins = calcAverage(44, 23, 71);
 let averageKoalas = calcAverage(65, 54, 49);
 
+// Given Data 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+averageDolphins = calcAverage(85, 54, 41);
+averageKoalas = calcAverage(23, 34, 27);
+
 const checkWinner = function (avgDolphins, avgKoalas) {
   if (averageDolphins >= 2 * averageKoalas) {
-    return `Dolphins win (${averageDolphins} vs. ${averageKoalas})`;
+    return `Dolphins win (${averageDolphins} vs. ${averageKoalas}).`;
   } else if (averageKoalas >= 2 * averageDolphins) {
-    return `Koalas win (${averageKoalas} vs. ${averageDolphins}13)`;
+    return `Koalas win (${averageKoalas} vs. ${averageDolphins}13).`;
   } else {
     return "No team wins!";
   }
 };
 
 console.log(checkWinner(averageDolphins, averageKoalas));
-
-// Given Data 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
