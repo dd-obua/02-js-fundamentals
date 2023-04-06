885e92972b45ee922b7ed2b044bf07d52504a111
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-06 20:13:51 +0300

    Add code representing a set of data as an array vs. as an object

---
+++ js/objects.js
@@ -0,0 +1,18 @@
+const currentYear = new Date().getFullYear();
+const birthYear = 1993;
+
+const denisArray = [
+  "Denis",
+  "Obua",
+  currentYear - birthYear,
+  "Software Engineer / Web Developer",
+  ["Geoffrey", "Peter", "Richard"]
+];
+
+const denisObject = {
+  firstName: "Denis",
+  lastName: "Obua",
+  age: currentYear - birthYear,
+  job: "Software Engineer / Web Developer",
+  friends: ["Geoffrey", "Peter", "Richard"]
+};
