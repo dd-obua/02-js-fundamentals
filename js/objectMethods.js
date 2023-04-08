9e483048cb5aff4dfc4d1efe16c9254ab4b38020
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-08 17:19:50 +0300

    Include getSummary method to construct a string from the object

--- js/objectMethods.js
+++ js/objectMethods.js
@@ -11,8 +11,16 @@ const dan = {
   calcAge: function () {
     this.age = currentYear - this.birthYear;
     return this.age;
+  },
+
+  getSummary: function () {
+    return `${this.firstName} is a ${this.age}-year old ${this.job}.`;
   }
 };
 
 console.log(dan.calcAge()); // method call is still necessary
 console.log("Dan's age:", dan.age);
+
+// Challenge: Contruct a string 'Denis is a 30-year old Developer' using a getSummary method
+
+console.log(dan.getSummary());
