5347bbc051a130c590341ee4aaf2ddd4a1a72630
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-08 16:44:08 +0300

    Create a method (a function of an object)

---
+++ js/objectMethods.js
@@ -0,0 +1,16 @@
+const currentYear = new Date().getFullYear();
+
+const dan = {
+  firstName: "Denis",
+  lastName: "Obua",
+  birthYear: 1993,
+  job: "Developer",
+  friends: ["Geoffrey", "Peter", "Richard"],
+  hasDriversLicence: false,
+
+  calcAge: function () {
+    return currentYear - this.birthYear;
+  }
+};
+
+ 
