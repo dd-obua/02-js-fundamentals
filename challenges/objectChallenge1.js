767c6a5a1b49b3b723912f2d71f3b0114aa636bb
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-10 18:02:59 +0300

    Simplify the calcBMI objects and call them outside the object

--- challenges/objectChallenge1.js
+++ challenges/objectChallenge1.js
@@ -1,36 +1,30 @@
 // Comparing BMI
-
-const markObj = {
+markObj = {
   firstName: "Mark",
   lastName: "Miller",
   mass: 78,
   height: 1.69,
   calcBMI: function () {
-    this.bmi = this.mass / this.height ** 2;
-    return this.bmi;
-  }
-};
-
-const johnObj = {
-  firstName: "John",
-  lastName: "Smith",
-  mass: 92,
-  height: 1.95,
-  calcBMI: function () {
-    this.bmi = this.mass / this.height ** 2;
-    return this.bmi;
+    return this.mass / this.height ** 2;
   }
 };
 
 const bmiMark = markObj.calcBMI().toFixed(1);
+
+johnObj = {
+  firstName: "John",
+  lastName: "Smith",
+  mass: 93,
+  height: 1.95,
+  calcBMI: function () {
+    return this.mass / this.height ** 2;
+  }
+};
+
 const bmiJohn = johnObj.calcBMI().toFixed(1);
 
-if (johnObj.bmi > markObj.bmi) {
-  console.log(
-    `${johnObj.firstName}'s BMI (${bmiJohn}) is higher than ${markObj.firstName}'s (${bmiMark})!`
-  );
+if (bmiJohn > bmiMark) {
+  console.log(`John's BMI (${bmiJohn}) is higher than Mark's (${bmiMark})!`);
 } else {
-  console.log(
-    `${markObj.firstName}'s BMI (${bmiMark}) is higher than ${johnObj.firstName}'s (${bmiJohn})!`
-  );
+  console.log(`Mark's BMI (${bmiMark}) is higher than John's (${bmiJohn})!`);
 }
