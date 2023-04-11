2a35c6c28f4ff980000a0f600f2597345a386bc8
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-11 11:55:35 +0300

    Calculate tips and totals given an array of bill values

---
+++ challenges/tip-calculator-loops.js
@@ -0,0 +1,15 @@
+// Tip calculator challenge using loops
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+
+for (let i = 0; i < bills.length - 1; i++) {
+  tips[i] = calcTip(bills[i]);
+  totals[i] = bills[i] + tips[i];
+  console.log(`Bill: ${bills[i]}, Tip: ${tips[i]}, Total: ${totals[i]}`);
+}
