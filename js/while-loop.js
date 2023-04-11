94d9d6be4fa051a6bb79a85ac56bc1ca8145ce33
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-11 11:34:02 +0300

    Roll a dice until 6 is got

--- js/while-loop.js
+++ js/while-loop.js
@@ -1,7 +1,15 @@
-
 let rep = 1;
 
 while (rep <= 10) {
   console.log(`Lift weight repetition ${rep}`);
   rep++;
+}
+
+// Roll dice
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+while (dice !== 6) {
+  console.log(`You rolled a ${dice}`);
+
+  dice = Math.trunc(Math.random() * 6) + 1;
 }
