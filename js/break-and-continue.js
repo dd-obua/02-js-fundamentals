ed54e546b66a9318ee31e6b4054cafcef820a8e8
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-11 08:28:49 +0300

    Read only string values from an array

---
+++ js/break-and-continue.js
@@ -0,0 +1,17 @@
+// break and continue statements
+
+const dan = [
+  "Denis",
+  "Obua",
+  2013 - 1993,
+  "Developer",
+  ["Peter", "Geoffrey", "Richard"],
+  true
+];
+
+// Read only string values from an array
+for (let i = 0; i < dan.length; i++) {
+  if (typeof dan[i] !== "string") continue;
+
+  console.log(`${typeof dan[i]}: ${dan[i]}`);
+}
