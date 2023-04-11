08a7b929e40b59464d9802c75a86a77e9c9036dc
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-11 08:08:07 +0300

    Read array elements

--- js/for-loop.js
+++ js/for-loop.js
@@ -3,3 +3,19 @@
 for (let rep = 1; rep <= 10; rep++) {
   console.log(`Weight lifting repetion ${rep}`);
 }
+
+console.log();
+
+// Looping through an array
+const dan = [
+  "Denis",
+  "Obua",
+  2013 - 1993,
+  "Developer",
+  ["Peter", "Geoffrey", "Richard"],
+  true
+];
+
+for (let i = 0; i < dan.length; i++) {
+  console.log(dan[i], typeof dan[i]);
+}
