d938419330f182ef32d467e4e96354a66e554e61
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-11 12:42:41 +0300

    Create a generic average function and use it to calculate the average of all the totals

--- challenges/tip-calculator-loops.js
+++ challenges/tip-calculator-loops.js
@@ -15,3 +15,19 @@ for (let i = 0; i < bills.length - 1; i++) {
 
   console.log(`Bill: ${bills[i]}, Tips: ${tips[i]}, Total: ${totals[i]}`);
 }
+
+console.log();
+
+// Average
+const calcAverage = function (arr) {
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+
+  return sum / arr.length;
+};
+
+const averageTotal = calcAverage(totals).toFixed(2);
+console.log("Average total:", averageTotal);
