8c29604a29d1f71785bb2d4678a9821c348c5198
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-11 11:18:01 +0300

    Nest loops

---
+++ js/nested-loops.js
@@ -0,0 +1,11 @@
+// Nest loops
+
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`---- Starting exercise ${exercise} ----`);
+
+  for (let rep = 1; rep < 6; rep++) {
+    console.log(`Lift weight repetition ${rep}`);
+  }
+
+  console.log();
+}
