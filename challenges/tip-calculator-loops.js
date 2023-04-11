23e438a4012b7c87b1a5a0ff7e7cf64260646d65
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-11 11:58:18 +0300

    Calculate the totals using push method

--- challenges/tip-calculator-loops.js
+++ challenges/tip-calculator-loops.js
@@ -10,6 +10,6 @@ const calcTip = function (bill) {
 
 for (let i = 0; i < bills.length - 1; i++) {
   tips[i] = calcTip(bills[i]);
-  totals[i] = bills[i] + tips[i];
+  totals.push(bills[i] + tips[i]);
   console.log(`Bill: ${bills[i]}, Tip: ${tips[i]}, Total: ${totals[i]}`);
 }
