379276733183b643a3a501201fac72b54617747f
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-11 08:22:29 +0300

    Compute ages for an array of birth years by pushing to an empty array

--- js/for-loop.js
+++ js/for-loop.js
@@ -22,7 +22,7 @@ for (let i = 0; i < dan.length; i++) {
   console.log(dan[i], typeof dan[i]);
 
   // Filling  types array
-  // types[i] = typeof dan[i];  
+  // types[i] = typeof dan[i];
 
   types.push(typeof dan[i]);
 }
@@ -30,3 +30,15 @@ for (let i = 0; i < dan.length; i++) {
 console.log();
 
 console.log(types);
+
+console.log();
+
+// Computing ages given an array of birth years
+const currentYear = new Date().getFullYear();
+const birthYears = [1990, 1991, 1992, 1993];
+const ages = [];
+
+for (let i = 0; i < birthYears.length; i++) {
+  ages.push(currentYear - birthYears[i]);
+}
+console.log(ages);
